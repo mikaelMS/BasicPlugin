53cbfe5a8aec60bd94369145df3389ad0ffa1109
mikaelMS <schmidt.m.work@googlemail.com>
2019-03-01 00:10:17 +0100

    Added assets and enqued mystyle.css

--- basic_learning_plugin/basic_learning_plugin.php
+++ basic_learning_plugin/basic_learning_plugin.php
@@ -18,13 +18,18 @@ defined('ABSPATH') or die();
 
 class MyPlugin
 {
+  // Normally contruct is only for initilizing for instance variable and not trigger actions
   function __construct() {
       add_action('init', array( $this, 'custom_post_type'));
   }
 
+  function register() {
+    add_action('admin_enqueue_scripts', array($this, 'enqueue'));
+  }
+
 	function activate() {
 		// generated a CPT
-    &this->custom_post_type();
+    $this->custom_post_type();
 		// flush rewrite rules
     flush_rewrite_rules(); //TODO: Check function
 	}
@@ -43,10 +48,17 @@ class MyPlugin
   function custom_post_type() {
     register_post_type('book', ['public' => true, 'label' => 'Michi Plugin' ] );
   }
+
+  function enqueue() {
+    // enqueue all our scripts
+    // TODO: Check function
+    wp_enqueue_style('mypluginstyle', plugins_url('/assets/mystyle.css', __FILE__));
+  }
 }
 
 if (class_exists(MyPlugin)) {
   $myPlugin = new MyPlugin();
+  $myPlugin->register();
 }
 
 // activation
@@ -56,4 +68,5 @@ register_activation_hook(__FILE__, array($myPlugin, ‘activate’));   // Need 
 register_deactivation_hook(__FILE__, array($myPlugin, ‘deactivate’));
 
 // unistall
+// Alternativ to making an uninstall.php file, but function need to be static
 // register_deactivation_hook(__FILE__, array($myPlugin, 'uninstall'));
